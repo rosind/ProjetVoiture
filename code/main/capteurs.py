8f440b5098671d431edc1aa530d40eb64f5826c3
tiboo200 <thibaud.danneau@std.heh.be>
2023-04-17 17:11:46 +0200

    test guigui

--- code/main/capteurs.py
+++ code/main/capteurs.py
@@ -22,25 +22,46 @@ class CapteurUltrason():
 
         self.trig = trig
         self.echo = echo
+    class CapteurUltrason():
+        def __init__(self, trig, echo):
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(trig,GPIO.IN)
+            GPIO.setup(echo, GPIO.OUT)
+
+            self.trig = trig
+            self.echo = echo
+
+            self.pulse_start = 0
+            self.pulse_end = 0
+            self.pulse_duree = 0
+
     def distance(self):
+        self.setup()
+        GPIO.output(self.trig, False)
+        time.sleep(0.0001)
         GPIO.output(self.trig, True)
         time.sleep(0.00001)
         GPIO.output(self.trig, False)
 
-        pulse_start = time.time()
-        pulse_end = time.time()
-
         while GPIO.input(self.echo)==0:
-            pulse_start = time.time()
+            self.pulse_start = time.time()
 
-        while GPIO.input(self.echo)==1:
-            pulse_end = time.time()
+        while GPIO.input(self.echo_pin)==1:
+            self.pulse_end = time.time()
 
-        pulse_duree = pulse_end - pulse_start
+        self.pulse_duree = self.pulse_end - self.pulse_start
 
-        distance = pulse_duree * 17150
+        distance = self.pulse_duree
+
+
+        distance = self.pulse_duree * 17150
         distance = round(distance, 2)
-        return distance
+
+        if distance > 2:
+            return distance - 0.5
+        else :
+            return 0
+
     def cleanup(self):
         GPIO.cleanup()
 
