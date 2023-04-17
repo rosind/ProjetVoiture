d6ad9ec4e48f37275d1301fcf697c6bb1340bc1b
tiboo200 <thibaud.danneau@std.heh.be>
2023-04-17 16:10:42 +0200

    Capteur

--- code/main/capteurs.py
+++ code/main/capteurs.py
@@ -14,3 +14,36 @@ class CapteurInfrarouge:
             else:
                 print("Je ne détecte pas la ligne noir !")
                 time.sleep(0.2)
+class CapteurUltrason():
+    def __init__(self, trig, echo):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(trig,GPIO.IN)
+        GPIO.setup(echo, GPIO.OUT)
+
+        self.trig = trig
+        self.echo = echo
+    def distance(self):
+        GPIO.output(self.trig, True)
+        time.sleep(0.00001)
+        GPIO.output(self.trig, False)
+
+        pulse_start = time.time()
+        pulse_end = time.time()
+
+        while GPIO.input(self.echo)==0:
+            pulse_start = time.time()
+
+        while GPIO.input(self.echo)==1:
+            pulse_end = time.time()
+
+        pulse_duree = pulse_end - pulse_start
+
+        distance = pulse_duree * 17150
+        distance = round(distance, 2)
+        return distance
+    def cleanup(self):
+        GPIO.cleanup()
+
+class CapteurRGBDetector:
+    """Capteur TCS3472"""
+    pass
--- code/main/main.py
+++ code/main/main.py
@@ -1,4 +1,11 @@
-from capteurs import CapteurInfrarouge
+from capteurs import *
 
-capt1 = CapteurInfrarouge(20)
-capt1.detect()
+#capt1 = CapteurInfrarouge(20)
+#capt1.detect()
+
+ultrason = CapteurUltrason(37,35)
+print("Attente du capteur...")
+time.sleep(2)
+distance = CapteurUltrason.distance()
+print("Distance ", distance, " cm" )
+ultrason.cleanup()
