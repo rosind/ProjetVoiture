70d7e4d97c1b6e1c7f946e5509264b35c33f53cd
Guillaume Rosin <99585997+rosind@users.noreply.github.com>
2023-04-18 12:58:41 +0200

    45

--- code/servo_test.py
+++ code/servo_test.py
@@ -2,7 +2,7 @@
 import PCA9685 as servo
 import time                  # Import necessary modules
 
-MinPulse = 250
+MinPulse = 50
 MaxPulse = 450
 
 def setup():
@@ -11,12 +11,21 @@ def setup():
     pwm.frequency = 50
 
 def servo_test():
-    for value in range(MinPulse, MaxPulse):
+    
+    for value in range(MinPulse, MaxPulse, 50):
         print ('PWM value: %d' % value)
         pwm.write(0, 0, value)
         #pwm.write(14, 0, value)
         #pwm.write(15, 0, value)
-        time.sleep(0.05)
+        time.sleep(0.1)
+
+    for value in range(MaxPulse, MinPulse, -50):
+        print ('PWM value: %d' % value)
+        pwm.write(0, 0, value)
+        #pwm.write(14, 0, value)
+        #pwm.write(15, 0, value)
+        time.sleep(0.1)
+
 
     pwm.write(0, 0, 350)
 
