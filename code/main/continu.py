78f4a6f83ede0e9a624884bf162e90dc32769e0d
Eva-Joly28 <129764237+Eva-Joly28@users.noreply.github.com>
2023-04-18 13:37:10 +0200

    Add files via upload

---
+++ code/main/continu.py
@@ -0,0 +1,13 @@
+from motorcc import *
+
+motor1 = motorcc(11,12)
+motor2 = motorcc(13,15)
+try:
+    while True:
+        motor1.setSpeed(50)
+        motor2.setSpeed(50)
+        motor2.advance()
+        motor1.advance()
+except KeyboardInterrupt:
+    motor1.clean()
+    motor2.clean()
