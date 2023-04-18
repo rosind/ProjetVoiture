bca6689b518198412fd987fcd2d23e720c2eeaf7
Guillaume Rosin <99585997+rosind@users.noreply.github.com>
2023-04-18 13:02:29 +0200

    48

--- code/servo_test.py
+++ code/servo_test.py
@@ -12,22 +12,24 @@ def setup():
 
 def servo_test():
     
-    for value in range(MinPulse, MaxPulse, 50):
-        print ('PWM value: %d' % value)
-        pwm.write(0, 0, value)
-        #pwm.write(14, 0, value)
-        #pwm.write(15, 0, value)
-        time.sleep(0.1)
+    while True:
 
-    for value in range(MaxPulse, MinPulse, -50):
-        print ('PWM value: %d' % value)
-        pwm.write(0, 0, value)
-        #pwm.write(14, 0, value)
-        #pwm.write(15, 0, value)
-        time.sleep(0.1)
+        for value in range(MinPulse, MaxPulse, 25):
+            print ('PWM value: %d' % value)
+            pwm.write(0, 0, value)
+            #pwm.write(14, 0, value)
+            #pwm.write(15, 0, value)
+            time.sleep(0.1)
+
+        for value in range(MaxPulse, MinPulse, -25):
+            print ('PWM value: %d' % value)
+            pwm.write(0, 0, value)
+            #pwm.write(14, 0, value)
+            #pwm.write(15, 0, value)
+            time.sleep(0.1)
 
 
-    pwm.write(0, 0, 350)
+        pwm.write(0, 0, 350)
 
 if __name__ == '__main__':
     setup()
