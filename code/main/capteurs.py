3e4571b2f4863fc58dc9edb7547f92f2c87d72ce
tiboo200 <thibaud.danneau@std.heh.be>
2023-04-19 15:03:36 +0200

    a

--- code/main/capteurs.py
+++ code/main/capteurs.py
@@ -42,11 +42,10 @@ class CapteurUltrason:
         pulseTotal= pulseEnd-pulseStart
         distance = pulseTotal*17150 #vitesse du son/2
         distance = round(distance,2)
-        print(distance)
-        #if (distance > 400):
-            #distance=40
-        #if (distance < 0):
-            #distance=0
+        if (distance > 400):
+            distance=40
+        if (distance < 0):
+            distance=0
 
         return distance
 
