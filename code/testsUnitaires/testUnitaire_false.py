dc8dcfd6aa2ae34bc80bd5a2632cf1aca2d4dcd8
tiboo200 <thibaud.danneau@std.heh.be>
2023-04-19 11:18:53 +0200

    infraFalse

--- code/testsUnitaires/testUnitaire_false.py
+++ code/testsUnitaires/testUnitaire_false.py
@@ -2,3 +2,11 @@ import unittest
 import sys
 sys.path.append('../main')
 from capteurs import CapteurInfrarouge
+
+class TestCapteurTrue(unittest.TestCase):
+    def test_capteurInfraIsTrue(self):
+        cap1 = CapteurInfrarouge(20)
+        self.assertFalse(cap1.detect())
+
+if __name__ == '__main__':
+    unittest.main()
