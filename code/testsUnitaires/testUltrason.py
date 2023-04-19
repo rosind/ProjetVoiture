a39c4eef0b759e7c64e9c83db5c16f5e84348848
tiboo200 <thibaud.danneau@std.heh.be>
2023-04-19 11:56:51 +0200

    cu

--- code/testsUnitaires/testUltrason.py
+++ code/testsUnitaires/testUltrason.py
@@ -1,14 +1,21 @@
 import unittest
+import sys
+sys.path.append('../main')
 from main.capteurs import CapteurUltrason
 
 class TestCapUltrason(unittest.TestCase):
-        
     def testInstanceUltrason(self):
+        ultrasoundRight = CapteurUltrason(19,26) #capteur droit
+        ultrasoundLeft = CapteurUltrason(11,9) #capteur gauche
+        ultrasoundFront =  CapteurUltrason(6,5) #avant
         self.assertIsInstance(ultrasoundFront, CapteurUltrason)
         self.assertIsInstance(ultrasoundRight, CapteurUltrason)
         self.assertIsInstance(ultrasoundLeft, CapteurUltrason)
     
     def detection(self):
+        ultrasoundRight = CapteurUltrason(19,26) #capteur droit
+        ultrasoundLeft = CapteurUltrason(11,9) #capteur gauche
+        ultrasoundFront =  CapteurUltrason(6,5) #avant
         self.assertGreater(ultrasoundFront.distance(),10)
         self.assertGreater(ultrasoundRight.distance(),10)
         self.assertGreater(ultrasoundLeft.distance(),10)        
@@ -16,7 +23,4 @@ class TestCapUltrason(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    ultrasoundRight = CapteurUltrason(19,26) #capteur droit
-    ultrasoundLeft = CapteurUltrason(11,9) #capteur gauche
-    ultrasoundFront =  CapteurUltrason(6,5) #avant
     unittest.main()
