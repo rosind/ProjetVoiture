f5c1527ce89c0b4375cdcc64c69f938523454d68
tiboo200 <thibaud.danneau@std.heh.be>
2023-04-19 16:17:21 +0200

    Borne min

--- code/testsUnitaires/testServo_ultrasson_borneMin.py
+++ code/testsUnitaires/testServo_ultrasson_borneMin.py
@@ -4,9 +4,9 @@ sys.path.append('../main')
 from capteurs import CapteurUltrason
 
 class TestCapteurClasse(unittest.TestCase):
-    def test_distanceMax120(self):
+    def test_distanceMin5(self):
         capt1 = CapteurUltrason(6,5) #6,5 --> Capteur avant
-        self.assertEqual(capt1.distance(),120)
+        self.assertEqual(capt1.distance(),5)
 
 if __name__ == '__main__':
     unittest.main()
