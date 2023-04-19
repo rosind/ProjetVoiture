9716d116e997de98462a9dc50d0c1a1d396d2653
Guillaume Rosin <99585997+rosind@users.noreply.github.com>
2023-04-19 14:16:04 +0200

    fff

--- code/main/servo.py
+++ code/main/servo.py
@@ -2,13 +2,45 @@
 import PCA9685 as servo
 import time                  # Import necessary modules
 
+class Servo:
+    def setup(self):
+        global pwm
+        self.pwm = servo.PWM()
+        self.pwm.frequency = 50
+        self.value=350
+
+    def rigth(self, a=60):
+        if self.value - a < self.value-75 or self.value + a > self.value+75:
+            print("Valeur comprise entre 250 et 450")
+        else:
+            self.pwm.write(1,0,self.value + a)
+            print("Tourne à droite", a, ' (', self.value + a, ')')
+
+    def left(self, a=60):
+        if self.value - a < self.value-75 or self.value + a > self.value+75:
+            print("Valeur comprise entre 250 et 450")
+        else: 
+            self.pwm.write(1,0,self.value - a)
+            print("Tourne à gauche", a, ' (', self.value + a, ')')
+
+    def servo_test(self):
+        if self.value < 300 or self.value > 420:
+            print("Valeur comprise entre 250 et 450")
+        else:
+            self.pwm.write(1,0,225)
+            time.sleep(1)
+            self.pwm.write(1,0,300)
+            time.sleep(1)
+            self.pwm.write(1,0,375)
+            time.sleep(1)
+            self.pwm.write(1,0,299)
+            
+
+"""
 MinPulse = 150
 MaxPulse = 450
 
-def setup():
-    global pwm
-    pwm = servo.PWM()
-    pwm.frequency = 50
+
 
 def servo_test():
     
@@ -30,7 +62,13 @@ def servo_test():
 
 
         pwm.write(0, 0, 350)
+"""
+
 
 if __name__ == '__main__':
-    setup()
-    servo_test()
+    MyServo=Servo()
+    MyServo.setup()
+    MyServo.servo_test()
+    #setup()
+    #Servo_test()
+
