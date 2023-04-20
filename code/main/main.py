39d49076ea1fbbdbc60fddc452ec1694ced7fd66
tiboo200 <thibaud.danneau@std.heh.be>
2023-04-20 12:22:01 +0200

    Update main.py

--- code/main/main.py
+++ code/main/main.py
@@ -4,12 +4,10 @@ from servo import *
 voit1 = Voiture(40)
 
 tour = int(input("Entre le nombre de tours:"))
-i=1
 while (tour > 0):
     voit1.suivreMur()
     if (voit1.th4.etat==True):
         tour-=1
-        i+=1
 
 print("Fin de course !")
 voit1.stopThread()
