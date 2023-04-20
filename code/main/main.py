b90a1c8a5cac194e04dec4f582d5769b06430f55
tiboo200 <thibaud.danneau@std.heh.be>
2023-04-20 14:29:13 +0200

    Update main.py

--- code/main/main.py
+++ code/main/main.py
@@ -6,7 +6,7 @@ voit1 = Voiture(40)
 tour = int(input("Entre le nombre de tours:"))
 while (tour > 0):
     voit1.suivreMur()
-    if voit1.th4.detectLine():
+    if voit1.detect_Line():
         tour-=1
 
 print("Fin de course !")
