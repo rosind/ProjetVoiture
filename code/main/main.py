74f2b111b13e2b6d5649106552fd30a633b0b4d8
tiboo200 <thibaud.danneau@std.heh.be>
2023-04-21 09:46:55 +0200

    Update main.py

--- code/main/main.py
+++ code/main/main.py
@@ -2,15 +2,18 @@ from Thread import *
 from servo import *
 
 voit1 = Voiture(60)
+try:
+    tour = int(input("Entre le nombre de tours:"))
+    while (tour > 0):
+        voit1.autonome()
+        print(voit1.detect_Line())
+        if voit1.detect_Line():
+            tour-=1
+            time.sleep(1)
 
-tour = int(input("Entre le nombre de tours:"))
-while (tour > 0):
-    voit1.autonome()
-    print(voit1.detect_Line())
-    if voit1.detect_Line():
-        tour-=1
-        time.sleep(1)
-
-print("Fin de course !")
-voit1.stopThread()
-voit1.stop_voiture()
+    print("Fin de course !")
+    voit1.stopThread()
+    voit1.stop_voiture()
+except(KeyboardInterrupt):
+    voit1.stopThread()
+    voit1.stop_voiture()
